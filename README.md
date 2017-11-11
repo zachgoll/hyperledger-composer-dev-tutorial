35f6990072ccad53a7cfdab29c031ee38034cfda
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2017-11-10 19:42:13 -0600

    Add video tutorial

---
+++ README.md
@@ -0,0 +1,116 @@
+# Hyperledger Composer Development Tutorial (MAC OSX)
+
+Use these commands while following along with [Part 5 of the Hyperledger Development Series]()
+
+## Prereqs (important!)
+
+See [first video in series](https://www.youtube.com/watch?v=nS_MRqAeEbQ) or visit the [official Mac setup documentation for Composer](https://hyperledger.github.io/composer/installing/prereqs-mac.html)
+
+Please note that if you have been following along with this tutorial series, you will need to run `nvm install --lts` again as Node has upgraded their long term support version.
+
+## Environment Setup Commands
+
+Download hyperledger client and tools (or `npm update` if you have already installed)
+
+```
+npm install -g composer-cli
+npm install -g generator-hyperledger-composer
+npm install -g composer-rest-server
+npm install -g yo
+```
+
+Clean up your docker containers (careful if using docker containers for other projects!)
+
+```
+docker kill $(docker ps -q)
+docker rm $(docker ps -aq)
+docker rmi $(docker images dev-* -q)
+```
+
+Clean up any composer credentials and connection profiles you might have created previously (again, this assumes you don't have any important composer projects already started!)
+
+```
+rm -rf ~/.composer-connection-profiles
+rm -rf ~/.composer-credentials
+```
+
+Start Hyperledger Fabric (this is what the business network connects to and is a series of Docker containers).
+
+```
+mkdir fabric-tools
+cd fabric-tools
+curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
+unzip fabric-dev-servers.zip
+./downloadFabric.sh
+./startFabric.sh
+./createPeerAdminCard.sh
+```
+
+## Project Setup/Deployment Commands
+
+Generate a template with Yo
+
+```
+yo hyperledger-composer:businessnetwork
+
+> Business network name: perishable-network
+> Description: A perishable tracking network
+> Author name:  zach
+> Author email: zach@email.com
+> License: Apache-2.0
+> Namespace: org.acme.shipping.perishable
+```
+
+This is based off of perishable sample network [found here](https://github.com/hyperledger/composer-sample-networks/tree/master/packages/perishable-network).
+
+Create your `.bna` file
+
+```
+mkdir dist
+composer archive create --sourceType dir --sourceName . -a ./dist/perishable-network.bna
+```
+
+Deploy the network
+
+```
+composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName perishable-network
+composer network start --card PeerAdmin@hlfv1 -A admin -S adminpw -a perishable-network.bna -f networkadmin.card
+composer card import networkadmin.card
+composer card list
+composer network ping --card admin@perishable-network
+```
+
+## The REST server
+
+To connect to REST server with authorization (Github), create new credentials for your application in github, and then put those credentials in the following environment variable and run it.
+
+Install github-authentication
+
+```
+npm install -g passport-github
+```
+
+Export your COMPOSER_PROVIDERS env variable
+
+```
+export COMPOSER_PROVIDERS='{
+  "github": {
+    "provider": "github",
+    "module": "passport-github",
+    "clientID": "d25c49eac0dbb40f09b1",                    
+    "clientSecret": "1041a7c497721de8e258b1a820d701664b07c698",               
+    "authPath": "/auth/github",
+    "callbackURL": "/auth/github/callback",
+    "successRedirect": "/",
+    "failureRedirect": "/"
+  }
+}'
+```
+
+Now, run the REST server with authentication
+
+```
+composer-rest-server -n perishable-network --card admin@perishable-network -a true -m true
+```
+
+You will need to import your identity card to use the REST server.
---
+++ test/logic.js
@@ -0,0 +1,227 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const AdminConnection = require('composer-admin').AdminConnection;
+const BrowserFS = require('browserfs/dist/node/index');
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
+const path = require('path');
+
+require('chai').should();
+let sinon = require('sinon');
+
+const bfs_fs = BrowserFS.BFSRequire('fs');
+const NS = 'org.acme.shipping.perishable';
+let grower_id = 'farmer@email.com';
+let importer_id = 'supermarket@email.com';
+let factory;
+let clock;
+
+describe('Perishable Shipping Network', () => {
+
+    let businessNetworkConnection;
+
+    before(() => {
+        BrowserFS.initialize(new BrowserFS.FileSystem.InMemory());
+        const adminConnection = new AdminConnection({ fs: bfs_fs });
+        return adminConnection.createProfile('defaultProfile', {
+            type: 'embedded'
+        })
+            .then(() => {
+                return adminConnection.connectWithDetails('defaultProfile', 'admin', 'Xurw3yU9zI0l');
+            })
+            .then(() => {
+                return BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+            })
+            .then((businessNetworkDefinition) => {
+                return adminConnection.deploy(businessNetworkDefinition);
+            })
+            .then(() => {
+                businessNetworkConnection = new BusinessNetworkConnection({ fs: bfs_fs });
+                return businessNetworkConnection.connectWithDetails('defaultProfile', 'perishable-network', 'admin', 'Xurw3yU9zI0l');
+            })
+            .then(() => {
+                // submit the setup demo transaction
+                // this will create some sample assets and participants
+                factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+                const setupDemo = factory.newTransaction(NS, 'SetupDemo');
+                return businessNetworkConnection.submitTransaction(setupDemo);
+            });
+    });
+
+    beforeEach(function () {
+        clock = sinon.useFakeTimers();
+    });
+
+    afterEach(function () {
+        clock.restore();
+    });
+
+    describe('#shipment', () => {
+
+        it('should receive base price for a shipment within temperature range', () => {
+            // submit the temperature reading
+            const tempReading = factory.newTransaction(NS, 'TemperatureReading');
+            tempReading.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+            tempReading.centigrade = 4.5;
+            return businessNetworkConnection.submitTransaction(tempReading)
+                .then(() => {
+                    // submit the shipment received
+                    const received = factory.newTransaction(NS, 'ShipmentReceived');
+                    received.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+                    return businessNetworkConnection.submitTransaction(received);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Grower');
+                })
+                .then((growerRegistry) => {
+                    // check the grower's balance
+                    return growerRegistry.get(grower_id);
+                })
+                .then((newGrower) => {
+                    // console.log(JSON.stringify(businessNetworkConnection.getBusinessNetwork().getSerializer().toJSON(newGrower)));
+                    newGrower.accountBalance.should.equal(2500);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Importer');
+                })
+                .then((importerRegistry) => {
+                    // check the importer's balance
+                    return importerRegistry.get(importer_id);
+                })
+                .then((newImporter) => {
+                    newImporter.accountBalance.should.equal(-2500);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getAssetRegistry(NS + '.Shipment');
+                })
+                .then((shipmentRegistry) => {
+                    // check the state of the shipment
+                    return shipmentRegistry.get('SHIP_001');
+                })
+                .then((shipment) => {
+                    shipment.status.should.equal('ARRIVED');
+                });
+        });
+
+        it('should receive nothing for a late shipment', () => {
+            // submit the temperature reading
+            const tempReading = factory.newTransaction(NS, 'TemperatureReading');
+            tempReading.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+            tempReading.centigrade = 4.5;
+            // advance the javascript clock to create a time-advanced test timestamp
+            clock.tick(1000000000000000);
+            return businessNetworkConnection.submitTransaction(tempReading)
+                .then(() => {
+                    // submit the shipment received
+                    const received = factory.newTransaction(NS, 'ShipmentReceived');
+                    received.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+                    return businessNetworkConnection.submitTransaction(received);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Grower');
+                })
+                .then((growerRegistry) => {
+                    // check the grower's balance
+                    return growerRegistry.get(grower_id);
+                })
+                .then((newGrower) => {
+                    // console.log(JSON.stringify(businessNetworkConnection.getBusinessNetwork().getSerializer().toJSON(newGrower)));
+                    newGrower.accountBalance.should.equal(2500);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Importer');
+                })
+                .then((importerRegistry) => {
+                    // check the importer's balance
+                    return importerRegistry.get(importer_id);
+                })
+                .then((newImporter) => {
+                    newImporter.accountBalance.should.equal(-2500);
+                });
+        });
+
+        it('should apply penalty for min temperature violation', () => {
+            // submit the temperature reading
+            const tempReading = factory.newTransaction(NS, 'TemperatureReading');
+            tempReading.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+            tempReading.centigrade = 1;
+            return businessNetworkConnection.submitTransaction(tempReading)
+                .then(() => {
+                    // submit the shipment received
+                    const received = factory.newTransaction(NS, 'ShipmentReceived');
+                    received.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+                    return businessNetworkConnection.submitTransaction(received);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Grower');
+                })
+                .then((growerRegistry) => {
+                    // check the grower's balance
+                    return growerRegistry.get(grower_id);
+                })
+                .then((newGrower) => {
+                    // console.log(JSON.stringify(businessNetworkConnection.getBusinessNetwork().getSerializer().toJSON(newGrower)));
+                    newGrower.accountBalance.should.equal(4000);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Importer');
+                })
+                .then((importerRegistry) => {
+                    // check the importer's balance
+                    return importerRegistry.get(importer_id);
+                })
+                .then((newImporter) => {
+                    newImporter.accountBalance.should.equal(-4000);
+                });
+        });
+
+        it('should apply penalty for max temperature violation', () => {
+            // submit the temperature reading
+            const tempReading = factory.newTransaction(NS, 'TemperatureReading');
+            tempReading.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+            tempReading.centigrade = 11;
+            return businessNetworkConnection.submitTransaction(tempReading)
+                .then(() => {
+                    // submit the shipment received
+                    const received = factory.newTransaction(NS, 'ShipmentReceived');
+                    received.shipment = factory.newRelationship(NS, 'Shipment', 'SHIP_001');
+                    return businessNetworkConnection.submitTransaction(received);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Grower');
+                })
+                .then((growerRegistry) => {
+                    // check the grower's balance
+                    return growerRegistry.get(grower_id);
+                })
+                .then((newGrower) => {
+                    // console.log(JSON.stringify(businessNetworkConnection.getBusinessNetwork().getSerializer().toJSON(newGrower)));
+                    newGrower.accountBalance.should.equal(5000);
+                })
+                .then(() => {
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.Importer');
+                })
+                .then((importerRegistry) => {
+                    // check the importer's balance
+                    return importerRegistry.get(importer_id);
+                })
+                .then((newImporter) => {
+                    newImporter.accountBalance.should.equal(-5000);
+                });
+        });
+    });
+});
